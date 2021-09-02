42065f303cba9c22da5f569bc8a32e6d591dfa8d
jjrryyaa <jjrryyaa@gmail.com>
2021-09-02 14:05:58 -0700

    fixed the updating

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class MyClient(discord.Client):
     async def send_results(self, embed: discord.Embed, content) -> None:
         for message in self.copyList:  # type:discord.Message
             if len(content) == 0:
-                await message.edit(embed=embed)
+                await message.edit(embed=embed, content="")
             else:
                 await message.edit(embed=embed, content=content)
 
@@ -31,6 +31,7 @@ class MyClient(discord.Client):
         newMessage = await message.channel.send("Will update next time a mvp is sent here")
         self.copyList.append(newMessage)
 
+    # Checks if I called for a placed flag
     async def on_message(self, message: discord.Message):
         if message.content.startswith(command + "placeFlag") and self.check_if_it_is_me(message):
             await self.set_flag_copy(message)
@@ -45,12 +46,14 @@ class MyClient(discord.Client):
     #             await self.send_results(message.embeds[0], message.content)
     #         await self.send_results(None, message.content)
 
+    # After called, will update all the messages in copyList to the current MVP message
     async def every_five_minutes(self):
         message = await self.get_channel(838492366121861150).fetch_message(872901421417246800)
         # message = await self.get_channel(875050700852310045).fetch_message(883071638193725471)
         if len(message.embeds) != 0:
             await self.send_results(message.embeds[0], message.content)
-        await self.send_results(None, message.content)
+        else:
+            await self.send_results(None, message.content)
         await asyncio.sleep(5*60)
         task = asyncio.create_task(self.every_five_minutes())
         await task
